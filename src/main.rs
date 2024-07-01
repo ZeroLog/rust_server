a325c54f298c1cb2c06150df49c09b01a43dc035
ZeroLog <YamaOG3@hotmail.com>
2024-07-01 15:54:31 +0700

    Added logic for selecting the first player

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
+extern crate rand;
 
- 
 fn handle_client(stream: TcpStream) -> std::io::Result<()> {
     let mut reader = BufReader::new(stream.try_clone()?);
     let mut writer = BufWriter::new(stream);
     let stdin = std::io::stdin();
     let mut input = String::new();
     let mut response = String::new();
-
+    let r;
+    if rand::random() {
+        r = 1;
+    } else {
+        r = 2;
+    }
+    writer.write_all(format!("{}\n", r).as_bytes())?;
+    writer.flush()?;
+   
     loop {
- 
-        response.clear();
-        reader.read_line(&mut response)?;
-        if response.is_empty() {
-            break;
-        }
-        
-        println!("Client: {}", response.trim());
 
-        input.clear();
-        stdin.read_line(&mut input)?;
-        writer.write_all(input.as_bytes())?;
-        writer.flush()?;
-       
+        if r == 1 {
+            println!("Ваш ход");
+            input.clear();
+            stdin.read_line(&mut input)?;
+            writer.write_all(input.as_bytes())?;
+            writer.flush()?;
+            println!("Ждите хода соперника");
+            response.clear();
+            reader.read_line(&mut response)?;
+            if response.is_empty() {
+                break;
+            }
+            println!("Client: {}", response.trim());
+        }
+        if r == 2 {
+            println!("Ждите хода соперника");
+            response.clear();
+            reader.read_line(&mut response)?;
+            if response.is_empty() {
+                break;
+            }
+            println!("Client: {}", response.trim());
+            println!("Ваш ход");
+            input.clear();
+            stdin.read_line(&mut input)?;
+            writer.write_all(input.as_bytes())?;
+            writer.flush()?;
+        }
     }
 
     Ok(())
