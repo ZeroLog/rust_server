d1bf8169490770e001607a04431364d66d5add0b
ZeroLog <YamaOG3@hotmail.com>
2024-07-05 13:52:34 +0700

    Added separate write and read functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
 extern crate rand;
 
@@ -8,7 +8,11 @@ fn handle_client(stream: TcpStream) -> std::io::Result<()> {
     let stdin = std::io::stdin();
     let mut input = String::new();
     let mut response = String::new();
+    let mut array: [Vec<i16>; 3] = [vec![1, 1, 1], vec![1, 1, 1], vec![1, 1, 1]];
     let r;
+
+
+
     if rand::random() {
         r = 1;
     } else {
@@ -16,36 +20,22 @@ fn handle_client(stream: TcpStream) -> std::io::Result<()> {
     }
     writer.write_all(format!("{}\n", r).as_bytes())?;
     writer.flush()?;
-   
+    print_array(&array);
     loop {
-
         if r == 1 {
             println!("Ваш ход");
-            input.clear();
-            stdin.read_line(&mut input)?;
-            writer.write_all(input.as_bytes())?;
-            writer.flush()?;
+            input = write_data(&stdin, &mut writer)?;
             println!("Ждите хода соперника");
-            response.clear();
-            reader.read_line(&mut response)?;
-            if response.is_empty() {
-                break;
-            }
+            response = read_data(&mut reader)?;
             println!("Client: {}", response.trim());
         }
+
         if r == 2 {
             println!("Ждите хода соперника");
-            response.clear();
-            reader.read_line(&mut response)?;
-            if response.is_empty() {
-                break;
-            }
+            response = read_data(&mut reader)?;
             println!("Client: {}", response.trim());
             println!("Ваш ход");
-            input.clear();
-            stdin.read_line(&mut input)?;
-            writer.write_all(input.as_bytes())?;
-            writer.flush()?;
+            input = write_data(&stdin, &mut writer)?;
         }
     }
 
@@ -69,4 +59,25 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+fn print_array(array: &[Vec<i16>; 3]) {
+    for row in array.iter() {
+        for val in row.iter() {
+            print!("{} ", val);
+        }
+        println!();
+    }
+}
+fn read_data(reader: &mut BufReader<TcpStream>) -> std::io::Result<String> {
+    let mut response = String::new();
+    reader.read_line(&mut response)?;
+    Ok(response)
+}
+
+fn write_data(stdin: &io::Stdin, writer: &mut BufWriter<TcpStream>) -> std::io::Result<String> {
+    let mut input = String::new();
+    stdin.read_line(&mut input)?;
+    writer.write_all(input.as_bytes())?;
+    writer.flush()?;
+    Ok(input.trim().to_string())
 }
